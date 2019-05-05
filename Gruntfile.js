3a69aaf4ce649f3eb0904f70d4d8a138ab528897
ValentinaKocharyan <valentinaqocharyan@gmail.com>
2019-05-05 12:56:24 -0700

    issue-13: configure build environment

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,4 +1,4 @@
-module.exports = function(grunt) {
+/*module.exports = function(grunt) {
 
     grunt.initConfig({
         concat: {
@@ -13,4 +13,55 @@ module.exports = function(grunt) {
     grunt.loadNpmTasks('grunt-contrib-concat');
 
     grunt.registerTask('default', ['concat']);
+};*/
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        concat: {
+            options: {
+                separator: ';'
+            },
+            dist: {
+                src: ['src/**/*.js'],
+                dest: 'dist/<%= pkg.name %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '/*! <%= pkg.name %> <%= grunt.template.today() %> */\n'
+            },
+            dist: {
+                files: {
+                    'dist/<%= pkg.name %>.min.js': ['<%= concat.dist.dest %>']
+                }
+            }
+        },
+        jshint: {
+            // define the files to lint
+            files: ['Gruntfile.js', 'src/**/*.js'],
+            // configure JSHint
+            options: {
+                // more options here if you want to override JSHint defaults
+                globals: {
+                    jQuery: false,
+                },
+                "esversion": 6,
+                expr: true
+            }
+        },
+        watch: {
+            files: ['<%= jshint.files %>'],
+            tasks: ['jshint', 'concat', 'uglify']
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+
+    grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
+
 };
