73ff3123c0e201e032327b3f90be325dae6caece
ValentinaKocharyan <valentinaqocharyan@gmail.com>
2019-05-05 13:00:31 -0700

    adding empty row in the end of gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,20 +1,3 @@
-/*module.exports = function(grunt) {
-
-    grunt.initConfig({
-        concat: {
-            "options": { "separator": ";" },
-            "build": {
-                "src": ["js/ms_script.js"],
-                "dest": "js/app.js"
-            }
-        }
-    });
-
-    grunt.loadNpmTasks('grunt-contrib-concat');
-
-    grunt.registerTask('default', ['concat']);
-};*/
-
 module.exports = function(grunt) {
 
     grunt.initConfig({
