3bedcaf79cdb12ac705919f9a8e82ee8240a6aa0
ValentinaKocharyan <valentinaqocharyan@gmail.com>
2019-05-07 12:27:48 -0700

    issue-13: load all grunt tasks with load-grunt-tasks package

--- Gruntfile.js
+++ Gruntfile.js
@@ -37,10 +37,7 @@ module.exports = function(grunt) {
         }
     });
 
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-contrib-concat');
+    require('load-grunt-tasks')(grunt);
 
     grunt.registerTask('default', ['jshint', 'concat', 'uglify']);
 
